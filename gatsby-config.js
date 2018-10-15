d608f40baaa8d75ca9cf6853b411cdc705a9c7ba
ShubuhU <patipon74@gmail.com>
2018-10-15 18:46:49 +0700

    update config (remove typography)

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,5 @@ module.exports = {
   siteMetadata: {
     title: 'ShubU Portfolio',
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+  
 }
